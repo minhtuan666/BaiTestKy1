970fe59bf0f94e9e6a230e4f0bd00d3bcb859e8e
minhtuan666 <leminhtuan1250@gmail.com>
2023-03-21 23:56:22 +0700

    baithiky1

--- Test1/formula.js
+++ Test1/formula.js
@@ -1,7 +1,7 @@
 function calculate() {
     const a = parseInt(document.getElementById("a").value);
     const b = parseInt(document.getElementById("b").value);
-    let sum = 1;
+    let sum = 0;
     for (let i = a; i <= b; i++) {
         if (isPrime(i)) {  
         sum += i;
